f6e4b41a06f6622a9352b8d7024a0dad2de8d88c
牛鑫语 <niuxinyu@szbit.cn>
2022-11-14 14:25:07 +0800

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    // quotes: [2, "single"],
+    // semi: [2, "never"],
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'none',
+        printWidth: 100
+      }
+    ]
+  }
+}
